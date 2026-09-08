664930b301443a73e2064812004a3638b46c943c
Harris <584400@bah.com>
2026-09-08 13:05:42 -0400

    PizzaGo initial commit

---
+++ PizzaGo/PizzaGo/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PizzaGo.Startup))]
+namespace PizzaGo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
